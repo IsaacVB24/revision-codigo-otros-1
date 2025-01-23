980bfcf4e53caeacdf9d08304cf91ab109896f71
Isaac Victoria <isaac127@live.com.mx>
2025-01-23 11:02:31 -0600

    Resolución del ejercicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,26 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+// Se declaran correctamente las constantes que hacen referencias a las etiquetas HTML mediante su etiqueta y clase
+const n = document.querySelector('p.name');
+const b = document.querySelector('p.blog');
+const l = document.querySelector('p.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+// Dado que se maneja con 'await', la función se declara asíncrona
+async function displayUser(username) {
+  n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  // Se declara una constante para recibir los datos de la API
+  const data = await response.json();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+
+  // Se añade if para mostrar la información del usuario si es que se encuentra, por lado contrario, se muestra un mensaje de no encontrado
+  if(data.status == 404) {
+    n.textContent = `No se encontró al usuario '${username}'.`;
+  } else {
+    n.textContent = `Name: ${data.name}`;
+    b.textContent = `URL blog: ${data.blog}`;
+    l.textContent = `Location: ${data.location}`;
+  }
 }
 
 function handleError(err) {
